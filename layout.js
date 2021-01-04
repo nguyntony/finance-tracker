67d56e0907cef34a277b53e1d23293b47a1e8d2a
Tony Nguyen <nguyen.tony1015@gmail.com>
2021-01-04 13:05:28 -0500

    decided to undo some organization decisions

--- layout.js
+++ layout.js
@@ -3,8 +3,6 @@ const layout = {
         header: '/partials/header',
         homeHeader: '/partials/homeHeader',
         footer: '/partials/footer',
-        loginForm: '/partials/loginForm',
-        signUpForm: '/partials/signUpForm'
     }
 };
 
