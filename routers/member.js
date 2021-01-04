157eaab379e9848349dc917b4cee46aabd8852de
Tony Nguyen <nguyen.tony1015@gmail.com>
2021-01-04 11:53:45 -0500

    inserted requireLogin middleware for all /member routes

--- routers/member.js
+++ routers/member.js
@@ -2,9 +2,10 @@ const express = require("express")
 const router = express.Router()
 
 const { memberController } = require("../controllers")
+const { userController } = require("../controllers")
 
 router
+    .use("/", userController.requireLogin)
     .get("/home", memberController.memberHomeController.home)
-
 
 module.exports = router
