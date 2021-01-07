1ff2abb1651b2a2f8877b853846f4b25428bcc67
Tyler Nguyen <nguyn.tyler@gmail.com>
2021-01-07 15:27:19 -0500

    edit imports

--- controllers/member/home.js
+++ controllers/member/home.js
@@ -1,26 +1,31 @@
-const { layout, partialContent, msgContent, getMessages } = require("../../layout")
+const {
+	layout,
+	partialContent,
+	msgContent,
+	getMessages,
+} = require("../../helper");
 
 const home = (req, res) => {
-    const { firstName } = req.session.user
+	const { firstName } = req.session.user;
 
-    res.render("member/home", {
-        // ...layout,
-        // locals: {
-        //     title: "Member Homepage",
-        //     firstName
-        // }
-        partials: {
-            ...partialContent,
-            ...msgContent
-        },
-        locals: {
-            title: "Member Homepage",
-            firstName,
-            messages: getMessages(req)
-        }
-    })
-}
+	res.render("member/home", {
+		// ...layout,
+		// locals: {
+		//     title: "Member Homepage",
+		//     firstName
+		// }
+		partials: {
+			...partialContent,
+			...msgContent,
+		},
+		locals: {
+			title: "Member Homepage",
+			firstName,
+			messages: getMessages(req),
+		},
+	});
+};
 
 module.exports = {
-    home
-}
+	home,
+};
