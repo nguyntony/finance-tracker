b22bd18f766e7780602cada43623a97a874293d3
Tyler Nguyen <nguyn.tyler@gmail.com>
2021-01-07 15:27:37 -0500

    edit imports

--- controllers/member/saving.js
+++ controllers/member/saving.js
@@ -1,40 +1,40 @@
-const { layout } = require("../../layout")
-const { Saving } = require('../../models')
+const { layout } = require("../../helper");
+const { Saving } = require("../../models");
 
 const showSavingForm = (req, res) => {
-    res.render("member/savingForm", {
-        ...layout,
-        locals: {
-            title: "Saving Form"
-        }
-    })
-}
+	res.render("member/savingForm", {
+		...layout,
+		locals: {
+			title: "Saving Form",
+		},
+	});
+};
 
 const processSavingForm = async (req, res) => {
-    const { id } = req.session.user
-    let { title, deadline, total, category } = req.body
+	const { id } = req.session.user;
+	let { title, deadline, total, category } = req.body;
 
-    if (!total) {
-        total = null
-    }
-    if (!deadline) {
-        deadline = null
-    }
+	if (!total) {
+		total = null;
+	}
+	if (!deadline) {
+		deadline = null;
+	}
 
-    const newSaving = await Saving.create({
-        title,
-        deadline,
-        total,
-        category,
-        uid: id,
-    })
-    req.session.flash = { success: "Your savings goal has been added." }
-    req.session.save(() => {
-        res.redirect("/member/home")
-    })
-}
+	const newSaving = await Saving.create({
+		title,
+		deadline,
+		total,
+		category,
+		uid: id,
+	});
+	req.session.flash = { success: "Your savings goal has been added." };
+	req.session.save(() => {
+		res.redirect("/member/home");
+	});
+};
 
 module.exports = {
-    showSavingForm,
-    processSavingForm
-}
+	showSavingForm,
+	processSavingForm,
+};
