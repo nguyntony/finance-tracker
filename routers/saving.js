586105d78fc0fc24646038c9d83f8cd28711f084
Tyler Nguyen <nguyn.tyler@gmail.com>
2021-01-08 11:40:07 -0500

    need to finished deletion form and process

--- routers/saving.js
+++ routers/saving.js
@@ -1,9 +1,17 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-const { memberController } = require("../controllers")
+const { memberController } = require("../controllers");
 
 router
-    .get("/list", memberController.savingController.list)
+	.get("/list", memberController.savingController.list)
+	.get(
+		"/edit/:savingId",
+		memberController.savingController.showEditSavingForm
+	)
+	.post(
+		"/edit/:savingId",
+		memberController.savingController.processEditSavingForm
+	);
 
-module.exports = router
+module.exports = router;
